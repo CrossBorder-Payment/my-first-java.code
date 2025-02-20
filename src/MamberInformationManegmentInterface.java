4c9b69a20af53ee755ca91f322809d3d6913ce27
Elyas <1650213507@qq.com>
2025-02-20 22:31:45 +0800

    Add files via upload

---
+++ src/MamberInformationManegmentInterface.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author haier
+ */
+public class MamberInformationManegmentInterface {
+    public static void main(String[] args)
+    {
+        System.out.println("—————————————————————————————");
+        System.out.println("|     会员信息管理          |");
+        System.out.println("|                           |");
+        System.out.println("| 会员编号 | 姓名 | 联系方式|");
+        System.out.println("|---------------------------|");
+        System.out.println("|   1  | 老刘 | 166xxxxxx   |");
+        System.out.println("|   2  | 老六 | 169xxxxxx   |");
+        System.out.println("|                           |");
+        System.out.println("|___________________________|");
+    }
+
+
+}
---
+++ src/SystemManagementInterface.java
@@ -0,0 +1,24 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author haier
+ */
+public class SystemManagementInterface {
+    public static void main(String[] args) {
+        System.out.println("--------------------------");
+        System.out.println("|    系统管理平台         |");
+        System.out.println("|                        |");
+        System.out.println("| 会员信息管理|  权限管理 |");
+        System.out.println("|                        |");
+        System.out.println("| 系统设置    |  数据备份 |");
+        System.out.println("|                        |");
+        System.out.println("|                        |");
+        System.out.println("|------------------------|");
+    }
+}
+  
